00f3e22bfc0e0ea76d87dda82060fb66f77bf6a8
Ayush Kumar Singh <91734803+ayush-kumarsingh@users.noreply.github.com>
2023-04-02 20:15:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,28 +11,6 @@ app.use(express.static(__dirname + '/static'))
 app.use(express.static(__dirname + '/images'))
 app.use(express.urlencoded())
 
-
-
-mongoose.connect('mongodb://localhost:27017/weather_contact_info')
-.then(()=>{
-    console.log('connected successfully');
-})
-.catch(()=>{
-    console.log('some error occured');
-})
-
-
-
-var userschema = mongoose.Schema({
-    name: String,
-    age: Number,
-    email: String,
-    message: String
-})
-var usermodel = mongoose.model('model', userschema);
-
-
-
 app.get('/', (req, res) => {
     res.status(200).sendFile('public/index.html', { root: __dirname });
 })
@@ -46,7 +24,23 @@ app.get('/contact', (req, res) => {
     res.status(200).sendFile('public/contact.html', { root: __dirname })
 })
 app.post('/contact', (req, res) => {
-    console.log(req.body);
+
+    //setting up the mongodb server, connecting to mongodb server
+    mongoose.connect('mongodb://localhost:27017/weather_contact_info')
+        .then(() => {
+            console.log('connected successfully');
+        })
+        .catch(() => {
+            console.log('the connection did not establish because the mongodb server was not turned on by the developer. Please ask the client to turn on the mongodb server...');
+        })
+    var userschema = mongoose.Schema({
+        name: String,
+        age: Number,
+        email: String,
+        message: String
+    })
+    var usermodel = mongoose.model('model', userschema);
+
     var data = new usermodel({
         name: req.body.urname,
         age: req.body.urage,
